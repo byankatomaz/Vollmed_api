26a3324fc5e5568d2faeb488a2c4419ef0a8d626
byankatomaz <byankatomaz@gmail.com>
2024-03-04 16:28:21 -0300

    feat: Validador para que agende a consulta com antecedencia

---
+++ api/src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.consulta.validacoes.agendamento;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+public class ValidadorHorarioAntecedencia implements ValidadorAgendamentoConsulta{
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+
+        if (diferencaEmMinutos < 30){
+            throw new ValidacaoException("Consulta deve ser agendada com no minimo 30 minutos de diferença");
+        }
+    }
+
+}
