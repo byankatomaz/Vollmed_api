0a0722736c6c69e80ffd5f9cdb647d249d8c192a
byankatomaz <byankatomaz@gmail.com>
2024-03-04 15:47:03 -0300

    feat: Model de medico

---
+++ api/src/main/java/med/voll/api/domain/medico/Medico.java
@@ -0,0 +1,33 @@
+package med.voll.api.domain.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.endereco.Endereco;
+
+@Table(name="medicos")
+@Entity(name="Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    private Boolean ativo;
+
+}
