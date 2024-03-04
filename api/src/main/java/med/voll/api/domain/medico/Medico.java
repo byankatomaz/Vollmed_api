160005fb63f75f499948be98af1d89e6752bf45a
byankatomaz <byankatomaz@gmail.com>
2024-03-04 16:44:48 -0300

    feat: Atualizar informações de medico

--- api/src/main/java/med/voll/api/domain/medico/Medico.java
+++ api/src/main/java/med/voll/api/domain/medico/Medico.java
@@ -40,6 +40,19 @@ public class Medico {
         this.ativo = true;
     }
 
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+
+        if (dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
 
 
 }
