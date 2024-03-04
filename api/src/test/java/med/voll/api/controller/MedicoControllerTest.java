a9de9a656b445964d036054c4edf552b2f0e62e9
byankatomaz <byankatomaz@gmail.com>
2024-03-04 16:33:49 -0300

    test: Teste do repositorio de medico/ cadastrar

---
+++ api/src/test/java/med/voll/api/controller/MedicoControllerTest.java
@@ -0,0 +1,12 @@
+package med.voll.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedicoControllerTest {
+
+    @Test
+    void cadastrar() {
+    }
+}
