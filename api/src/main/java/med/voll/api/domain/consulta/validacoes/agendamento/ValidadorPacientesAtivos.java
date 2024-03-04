32d1eb9e244b20f3acc9cd247281b55875c9bedc
byankatomaz <byankatomaz@gmail.com>
2024-03-04 16:29:10 -0300

    feat: Validador para pacientes ativos

---
+++ api/src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorPacientesAtivos.java
@@ -0,0 +1,22 @@
+package med.voll.api.domain.consulta.validacoes.agendamento;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorPacientesAtivos implements ValidadorAgendamentoConsulta{
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var pacienteAtivo = pacienteRepository.findAtivoById(dados.idPaciente());
+
+        if (!pacienteAtivo){
+            throw new ValidacaoException("O paciente está inativo, não poderá agendar consultas");
+        }
+    }
+}
