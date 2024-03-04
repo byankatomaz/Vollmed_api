af1bd3e0bb32463a582c89735a442cff051c6f94
byankatomaz <byankatomaz@gmail.com>
2024-03-04 16:06:19 -0300

    feat: DTO para retornar os dados de cadastro paciente

---
+++ api/src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,31 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.endereco.DadosEndereco;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record DadosCadastroPaciente(
+
+        @NotBlank
+        String nome,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String telefone,
+
+        @NotBlank
+        @CPF
+        String cpf,
+
+        @NotNull
+        @Valid
+        DadosEndereco endereco
+
+) {
+}
