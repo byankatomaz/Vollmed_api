ca66c37da3e7a4bc65bbd6208bcd4092b725d42d
byankatomaz <byankatomaz@gmail.com>
2024-03-04 16:31:22 -0300

    feat: Dto de dados para retornar detalhes da consulta

---
+++ api/src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -0,0 +1,16 @@
+package med.voll.api.domain.consulta;
+
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+public record DadosDetalhamentoConsulta(
+        Long id,
+        Long idMedico,
+        Long idPaciente,
+        LocalDateTime data
+) {
+    public DadosDetalhamentoConsulta(Consulta consulta) {
+        this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData());
+    }
+}
