facf49436ab6276e37d2b0968cf7dc045910fde7
byankatomaz <byankatomaz@gmail.com>
2024-03-04 15:54:53 -0300

    feat: Repositorio de Consulta

---
+++ api/src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -0,0 +1,14 @@
+package med.voll.api.domain.consulta;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.nio.channels.FileChannel;
+import java.time.LocalDateTime;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    boolean existsByMedicoIdAndDataAndMotivoCancelamentoIsNull(Long idMedico, LocalDateTime data);
+
+    boolean existsByPacienteIdAndDataBetween(Long aLong, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
+}
